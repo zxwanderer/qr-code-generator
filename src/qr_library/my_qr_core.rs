105abede090c53476b25baa6b93fdbccc8b1f42b
zxwanderer <no@mail.com>
2024-08-04 09:35:35 +0300

    some updates

--- src/qr_library/my_qr_core.rs
+++ src/qr_library/my_qr_core.rs
@@ -1,5 +1,7 @@
-use crate::qr_library::defines::MyImageBuffer;
+use crate::qr_library::defines::{BytesVector, MyImageBuffer};
 use crate::qr_library::{convert_to_u8, generator, ZxQrCode};
+use image::ImageFormat;
+use std::io::{BufWriter, Cursor};
 
 pub struct MyQrCore {
     image_buffer: MyImageBuffer,
@@ -8,6 +10,19 @@ pub struct MyQrCore {
 impl ZxQrCode for MyQrCore {
     fn convert_to_zx_quad(&self) -> Vec<u8> {
         convert_to_u8::convert(&self.image_buffer)
+    }
+
+    fn convert_to_image(&self, file_format: &String) -> Result<Vec<u8>, String> {
+        let image_format = ImageFormat::from_extension(file_format).unwrap();
+        let mut c = Cursor::new(Vec::new());
+        match self
+            .image_buffer
+            .write_to(&mut c, image_format)
+            .map_err(|e| e.to_string())
+        {
+            Ok(()) => Ok(c.into_inner()),
+            Err(e) => Err(e),
+        }
     }
 
     fn save_to_image(&self, path: &String) -> Result<(), String> {
